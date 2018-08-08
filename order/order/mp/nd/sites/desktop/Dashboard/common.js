8afb09374479ef5dcbdc5f3349b711ccec942988
haodong Ju <juhaodong@gmail.com>
2018-08-08 12:37:29 +0200

    update

--- order/order/mp/nd/sites/desktop/Dashboard/common.js
+++ order/order/mp/nd/sites/desktop/Dashboard/common.js
@@ -55,24 +55,8 @@ function getQueryString(name) {
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
 }
-function setCookie(name,value,Days)
-{
 
-    var exp　= new Date();
-    exp.setTime(exp.getTime() +Days*24*60*60*1000);
-    document.cookie = name +"="+ decodeURI(value) + ";expires=" + exp.toGMTString();
-    console.log(document.cookie);
 
-}
-function getCookie(name)
-{
-    var arr =document.cookie.match(new RegExp("(^|)"+name+"=([^;]*)(;|$)"));
-    if(arr !=null) return decodeURIComponent(arr[2]); return null;
-}
-function deleteCookie(name) {
-    this.setCookie(name,'',-1);
-
-}
 function timeNow(){
     var years,months,days,hours, minutes, seconds;
     var intYears,intMonths,intDays,intHours, intMinutes, intSeconds;
